8b8d52a27f29e2351680cc0b1a5a46d9f8ef2200
VaibhavDS19 <62388173+VaibhavDS19@users.noreply.github.com>
2020-08-31 01:06:22 +0530

    Delete index.js

--- Day1/Assignment4/index.js
+++
@@ -1,9 +0,0 @@
-function a4(strs)
-{
-   for(let i=0;i<strs.length;i++)
-   {
-      if(strs[i].indexOf('a')>=0)
-         console.log(strs[i])
-   }
-}
-a4(['apples', 'bananas', 'oranges', 'fruits'])
